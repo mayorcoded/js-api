3acc5a2211e67e5ce66f93ddac05e9630d5ffb3f
Mayowa <mtudonu85@gmail.com>
2019-05-02 23:39:07 +0100

    chnage mysql connection to pool

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -5,8 +5,12 @@ let connection;
 
 async function dbConnection(){
     try {
-        connection = await mysql.createConnection({
-            host: config.db_host, user: config.db_user, password: config.db_password, database: config.db_name
+        connection = await mysql.createPool({
+            host: config.db_host, 
+            user: config.db_user, 
+            password: config.db_password, 
+            database: config.db_name,
+            connectionLimit: 10
         });
         return connection;
     } catch (error) {
